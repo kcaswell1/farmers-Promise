f63475b8e7cbf21e2856e3ae3ea24534b0ef01a4
kcaswell1 <50675382+kcaswell1@users.noreply.github.com>
2019-06-19 18:44:18 -0400

    Add files via upload

---
+++ css/main.js
@@ -0,0 +1,92 @@
+///////////* this is for the milk *///////////////
+
+function milkPrice1() {
+  
+    var x = document.getElementById("pint").value;
+    document.getElementById("price").innerHTML = x
+    
+  }
+  
+  function milkPrice2() {
+    
+    var x = document.getElementById("quart").value;
+    document.getElementById("price").innerHTML = x
+    
+  }
+  
+  function milkPrice3(){
+    
+    var x = document.getElementById("half").value;
+    document.getElementById("price").innerHTML = x
+    
+  }
+
+  function milkPrice4() {
+  
+    var x = document.getElementById("gallon").value;
+    document.getElementById("price").innerHTML = x
+    
+  }
+
+  function milkPrice5() {
+  
+    var x = document.getElementById("choc").value;
+    document.getElementById("price").innerHTML = x
+    
+  }
+  
+
+//////////* this is for the eggs *////////////////
+
+function eggPrice1() {
+  
+    var x = document.getElementById("6-egg").value;
+    document.getElementById("price").innerHTML = x
+    
+  }
+  
+  function eggPrice2() {
+    
+    var x = document.getElementById("12-egg").value;
+    document.getElementById("price").innerHTML = x
+    
+  }
+  
+  function eggPrice3(){
+    
+    var x = document.getElementById("18-egg").value;
+    document.getElementById("price").innerHTML = x
+    
+  }
+
+  ///////* THIS IS FOR THE YOGURT *//////////////////
+
+  function yogurtPrice1() {
+  
+    var x = document.getElementById("plain").value;
+    document.getElementById("price").innerHTML = x
+    
+  }
+  
+  function yogurtPrice2() {
+    
+    var x = document.getElementById("van").value;
+    document.getElementById("price").innerHTML = x
+    
+  }
+  
+  function yogurtPrice3(){
+    
+    var x = document.getElementById("cherry").value;
+    document.getElementById("price").innerHTML = x
+    
+  }
+
+  function yogurtPrice1() {
+  
+    var x = document.getElementById("blue").value;
+    document.getElementById("price").innerHTML = x
+    
+  }
+  
+////////// THIS IS FOR GETTING THE PRICE ///////////////
